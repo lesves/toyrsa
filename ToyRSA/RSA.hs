00560570026103d00b38256cbc69c2c4c3ba8fd6
Lukáš Veškrna <lukas.veskrna@gmail.com>
2024-05-30 22:08:07 +0200

    Check e validity

--- ToyRSA/RSA.hs
+++ ToyRSA/RSA.hs
@@ -8,16 +8,21 @@ data PublicKey = PublicKey { pubN :: Integer, pubE :: Integer } deriving (Show, 
 data PrivateKey = PrivateKey { privN :: Integer, privD :: Integer } deriving (Show, Read, Eq)
 
 generateKeys :: (RandomGen g) => Int -> g -> ((PublicKey, PrivateKey), g)
-generateKeys bits rand = ((PublicKey { pubN = n, pubE = e }, PrivateKey { privN = n, privD = d }), rand'') where
-    (p, rand') = generatePrime (bits `div` 2) rand
-    (q, rand'') = generatePrime (bits `div` 2) rand'
-    n = p*q
-    l_n = lcm (p-1) (q-1)
-    -- todo: check e is not divisor of l_n
-    e = 2^16 + 1
-    d = case inv e l_n of
-        Nothing -> error "invalid state"
-        Just x -> x
+generateKeys bits rand = if valid 
+        then ((PublicKey { pubN = n, pubE = e }, PrivateKey { privN = n, privD = d }), rand'')
+        else generateKeys bits rand''
+    where
+        (p, rand') = generatePrime (bits `div` 2) rand
+        (q, rand'') = generatePrime (bits `div` 2) rand'
+        n = p*q
+        l_n = lcm (p-1) (q-1)
+        e = 2^16 + 1
+        d = case inv e l_n of
+            Nothing -> error "invalid state"
+            Just x -> x
+
+        -- e must not be a divisor of l_n
+        valid = l_n `rem` e /= 0
 
 encrypt :: PublicKey -> Integer -> Integer
 encrypt key m = modExp m (pubE key) (pubN key)
